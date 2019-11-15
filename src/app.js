2bf53c1701f3ef7aad9d00717e3cd96486659312
Quasar Wei <quasar.wei@outlook.com>
2019-11-15 07:51:43 -0800

    C:/Program Files/Git/api/cat and /api/dog endpoints

---
+++ src/app.js
@@ -0,0 +1,59 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+
+app.get('/api/cat', (req, res) => {
+  const cat = {
+    imageURL:
+      'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+    imageDescription:
+      'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Fluffy',
+    sex: 'Female',
+    age: 2,
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+  };
+  res.json(cat);
+});
+
+app.get('/api/dog', (req, res) => {
+  const dog = {
+    imageURL:
+      'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription:
+      'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  };
+  res.json(dog);
+});
+
+// Catch-all 404
+app.use(function(req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Catch-all Error handler
+// Add NODE_ENV check to prevent stacktrace leak
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: app.get('env') === 'development' ? err : {}
+  });
+});
+
+module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,26 +1,8 @@
-const express = require('express');
-const cors = require('cors');
+require('dotenv').config();
 
-const app = express();
-app.use(cors());
+const app = require('./app');
+const { PORT, DATABASE_URL } = require('./config');
 
-// Catch-all 404
-app.use(function (req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// Catch-all Error handler
-// Add NODE_ENV check to prevent stacktrace leak
-app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: app.get('env') === 'development' ? err : {}
-  });
-});
-
-app.listen(8080,()=>{
-  console.log('Serving on 8080');
+app.listen(PORT,()=>{
+  console.log(`Server listening at http://localhost:${PORT}`);
 });
