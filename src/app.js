199352f4065a3cc433c873a27117b6c6a3a163b0
Quasar Wei <quasar.wei@outlook.com>
2019-11-15 08:52:24 -0800

    auth

--- src/app.js
+++ src/app.js
@@ -9,6 +9,16 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({error: 'Unauthorized Request' });
+  }
+  next();
+});
+
 app.get('/api/cat', (req, res) => {
   const cat = {
     imageURL:
--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL || '',
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || '',
-}
+  API_TOKEN: process.env.API_TOKEN || 'dummy token'
+};
