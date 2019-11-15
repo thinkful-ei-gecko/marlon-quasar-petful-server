2203f2de1dd50d310a30b6949c1bfca44c758a13
Marlon Agno <mdj.agno@gmail.com>
2019-11-15 14:06:39 -0500

    created Dogs and People queues

--- utility/Dogs.js
+++ utility/Dogs.js
@@ -7,7 +7,7 @@ let Dogs = new Queue;
 const dogs = [
   {
     imageURL:
-    'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+    'https://boygeniusreport.files.wordpress.com/2016/11/puppy-dog.jpg?quality=98&strip=all&w=782',
     imageDescription:
     'Orange bengal cat with black stripes lounging on concrete.',
     name: 'Fluffy',
@@ -19,7 +19,7 @@ const dogs = [
   },
   {
     imageURL:
-    'https://images2.minutemediacdn.com/image/upload/c_crop,h_1193,w_2121,x_0,y_64/f_auto,q_auto,w_1100/v1565279671/shape/mentalfloss/578211-gettyimages-542930526.jpg',
+    'https://i.ytimg.com/vi/BDxx1TnWTX0/maxresdefault.jpg',
     imageDescription:
     'Orange bengal cat with black stripes lounging on concrete.',
     name: 'Fluffy',
@@ -31,7 +31,7 @@ const dogs = [
   },
   {
     imageURL:
-    'https://upload.wikimedia.org/wikipedia/commons/6/66/An_up-close_picture_of_a_curious_male_domestic_shorthair_tabby_cat.jpg',
+    'https://cdn.theatlantic.com/assets/media/img/mt/2019/07/GettyImages_138965532/lead_720_405.jpg?mod=1563813032',
     imageDescription:
     'Orange bengal cat with black stripes lounging on concrete.',
     name: 'Fluffy',
@@ -43,7 +43,7 @@ const dogs = [
   },
   {
     imageURL:
-    'https://www.sciencenewsforstudents.org/sites/default/files/scald-image/860_cat_names.png',
+    'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13002253/GettyImages-521536928-_1_.jpg',
     imageDescription:
     'Orange bengal cat with black stripes lounging on concrete.',
     name: 'Fluffy',
@@ -55,7 +55,7 @@ const dogs = [
   },
   {
     imageURL:
-    'https://cbsnews1.cbsistatic.com/hub/i/2016/03/23/38e32f54-b910-4612-8852-be9e0fbdbf73/cat-istock.jpg',
+    'https://article.images.consumerreports.org/f_auto/prod/content/dam/CRO%20Images%202018/Health/March/CR-Health-InlineHero-toxic-food-for-dogs-0318',
     imageDescription:
     'Orange bengal cat with black stripes lounging on concrete.',
     name: 'Fluffy',
@@ -67,6 +67,8 @@ const dogs = [
   },
 ];
 
-cats.map(cat => Cats.enqueue(cat));
+dogs.map(dog => Dogs.enqueue(dog));
 
-Cats.display();
+Dogs.display();
+
+module.exports = Dogs;
