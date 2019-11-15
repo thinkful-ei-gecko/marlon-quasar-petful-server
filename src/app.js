d3c30126327f4a2aeec1cf61c5886b8f8198bda1
Marlon Agno <mdj.agno@gmail.com>
2019-11-15 17:38:23 -0500

    removed token validation temporarily

--- src/app.js
+++ src/app.js
@@ -15,15 +15,15 @@ app.use(cors({
   origin: 'http://localhost:3000' 
 }));
 
-app.use(function validateBearerToken(req, res, next) {
-  const apiToken = process.env.API_TOKEN;
-  const authToken = req.get('Authorization');
+// app.use(function validateBearerToken(req, res, next) {
+//   const apiToken = process.env.API_TOKEN;
+//   const authToken = req.get('Authorization');
 
-  if (!authToken || authToken.split(' ')[1] !== apiToken) {
-    return res.status(401).json({error: 'Unauthorized Request' });
-  }
-  next();
-});
+//   if (!authToken || authToken.split(' ')[1] !== apiToken) {
+//     return res.status(401).json({error: 'Unauthorized Request' });
+//   }
+//   next();
+// });
 
 app.use('/api/cat', catRouter);
 app.use('/api/dog', dogRouter);
